435e4c569b21ecb761158ee338c5994b96ceb697
monirozzamanroni <monirozzaman35-223@diu.edu.bd>
2020-08-17 11:59:00 +0600

    Whole project switch to Springboot Framework

--- eLearner_app/courses/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Courses,Learnerns,courseReviews
-# Register your models here.
-
-admin.site.register(Courses)
-admin.site.register(Learnerns)
-admin.site.register(courseReviews)
--- eLearner_app/courses/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import CourseSerializer
-from .models import Courses
-# Create your views here.
-
-class CourseViewSet(viewsets.ModelViewSet):
-    queryset = Courses.objects.all()
-    serializer_class = CourseSerializer
